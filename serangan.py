943be98080bfee08ee5f625cd0c472ca991295ac
Herlangga Yusuf Syailendra <herlangga72@gmail.com>
2020-11-27 13:31:55 +0700

    Add files via upload

--- serangan.py
+++ serangan.py
@@ -10,8 +10,10 @@ def make_request():
     br.find_control(name="nama",type='text',nr=1).value=str(random.randint(1000000,10000000))
     br.find_control(name="rek_pengirim").value=str(random.randint(1000000,10000000))
     br.find_control(name="userfile").add_file(open("ini file palsu.txt"),'img/png',"bukti tf")
-    br.submit()
-    print('berhasil '+str(ppp))
+    b = br.submit()
+    # print(b.read())
+    if b is not None:
+      print('berhasil '+str(ppp))
 class myThread (threading.Thread):
    def __init__(self, threadID, name, counter):
       threading.Thread.__init__(self)
@@ -21,16 +23,12 @@ class myThread (threading.Thread):
    def run(self):
       print("start")
       make_request()
-
-# Create new threads
 thread1 = myThread(1, "Thread-1", 1)
 thread2 = myThread(2, "Thread-2", 2)
 thread3 = myThread(3, "Thread-2", 3)
 thread4 = myThread(4, "Thread-2", 4)
 thread5 = myThread(5, "Thread-2", 5)
 thread6 = myThread(6, "Thread-2", 6)
-
-# Start new Threads
 thread1.start()
 thread2.start()
 thread3.start()
